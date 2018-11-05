c888d96e39e7c75f468b0725a4985702d1efddaf
Gabriel <developer.gabriel@gmail.com>
2018-11-05 16:45:41 -0200

    Configuring basic route and adding logic for movie listing.

--- world-cup-spa/src/main.js
+++ world-cup-spa/src/main.js
@@ -1,17 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import VueResource from 'vue-resource';
-import BootstrapVue from 'bootstrap-vue'
-import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue';
+import VueRouter from 'vue-router';
+import  { routes } from "./routes";
 
 Vue.use(BootstrapVue);
 Vue.use(VueResource);
 Vue.use(VueRouter);
+
+const router = new VueRouter( { routes });
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
 el: '#app',
+router,
 render: h => h(App)
 })
---
+++ world-cup-spa/src/routes.js
@@ -0,0 +1,7 @@
+import Index from "./components/Index.vue";
+
+export const routes = [
+    {
+        path: '', component: Index
+    }
+]
