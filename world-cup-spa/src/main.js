3f8ff21b1ab112217e8e5954762b83c36627107b
Gabriel <developer.gabriel@gmail.com>
2018-11-05 14:33:46 -0200

    Importing and registering dependencies.

--- world-cup-spa/src/main.js
+++ world-cup-spa/src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+
+Vue.use(BootstrapVue);
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+el: '#app',
+render: h => h(App)
 })
