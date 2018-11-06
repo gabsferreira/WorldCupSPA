aab2db052c774239b736e1571ff3dacd93b04026
Gabriel <developer.gabriel@gmail.com>
2018-11-06 13:37:54 -0200

    Adding Cup component and its logic.

---
+++ world-cup-spa/src/components/Cup.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <b-jumbotron header="World Cup" lead="Bootstrap 4 Components for Vue.js 2" >
+        <p>For more information visit website</p>
+        </b-jumbotron>
+        <b-card-group columns>
+            <div>
+                <b-card>
+                <p>{{ this.cup.champion }}</p>
+                <p>{{ this.cup.secondPlace }}</p>
+                </b-card>
+            </div>
+        </b-card-group>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      cup: {}
+    }
+  },
+
+  created() {
+    let promise = this.$http.get('https://localhost:44303/api/cup/' + this.$route.params.id);
+
+    promise.then(res => res.json())
+      .then(cup => this.cup = cup, err => console.log(err));
+  }
+
+  }
+</script>
+
+<style>
+</style>
--- world-cup-spa/src/routes.js
+++ world-cup-spa/src/routes.js
@@ -1,7 +1,7 @@
 import Index from "./components/Index.vue";
+import Cup from "./components/Cup.vue";
 
 export const routes = [
-    {
-        path: '', component: Index
-    }
+    { path: '', component: Index },
+    { path: '/Cup/:id', component: Cup }
 ]
